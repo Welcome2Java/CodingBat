ab7d19ccddfab22f71dde8526161e1cc8382a97c
JonJoo57 <47547628+JonJoo57@users.noreply.github.com>
2020-03-21 20:40:37 -0500

    3/21/2020 update

--- src/BasicString_2/BasicString_2.java
+++ src/BasicString_2/BasicString_2.java
@@ -142,4 +142,27 @@ public class BasicString_2 {
 		  return false;
 		}
 
+	public String repeatSeparator(String word, String sep, int count) {
+		String result = "";
+		for (int i = 0; i < count; i++) {
+			result = result + word;
+			if (i != (count - 1)) {
+				result = result + sep;
+			}
+		}
+		return result;
+	}
+	
+	public String repeatEnd(String str, int n) {
+		String result = "";
+		String extract = str.substring(str.length()-n, str.length());
+		for(int i=0; i< n; i++) {
+			result = result + extract;
+		}
+		
+		
+		return result;
+	}
+
+
 }
